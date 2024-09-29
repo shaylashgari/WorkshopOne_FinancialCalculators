1cfac7d699ce9585b800e75ebf3c1de2f9ca2c8a
Shay <shaylashgari@gmail.com>
2024-09-29 17:30:41 -0500

    latest

--- src/main/java/com/ps/FinancialCalculator.java
+++ src/main/java/com/ps/FinancialCalculator.java
@@ -3,16 +3,23 @@ package com.ps;
 import java.util.Scanner;
 
 
-
-public class MortgageCalculator {
+public class FinancialCalculator {
     public static void main(String[] args) {
+
+        int calculatorChoice;
+
+        Scanner input= new Scanner(System.in);
+        System.out.println("Please enter which calculator you would like to use:");
+        calculatorChoice= input.nextInt();
+
+
         float principleAmount;
         float interestRate;
         int loanLength;
+        double totalInterest;
 
         Scanner input= new Scanner(System.in);
         System.out.println(" Please enter the principle amount:");
-
         principleAmount= input.nextFloat();
         System.out.println("Please enter the interest rate: ");
         interestRate= input.nextFloat();
@@ -25,7 +32,15 @@ public class MortgageCalculator {
         double monthlyPayment = (principleAmount * monthlyInterestRate * Math.pow(1+monthlyInterestRate, loanLengthInMonths))/
                 (Math.pow(1+monthlyInterestRate, loanLengthInMonths)-1);
 
+        int n = 1;
+        int t= 15;
+
+        totalInterest = principleAmount * Math.pow((1+ (interestRate/100)/n), n*t);
+                // A=P(1+r/n)^nt
+
         System.out.println("The monthly payment is: "+ monthlyPayment);
+
+        System.out.println( "The total interest:"+ totalInterest);
 
     }
 
